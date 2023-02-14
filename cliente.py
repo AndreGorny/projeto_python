4e0adb33923bf57a1685cfd1ccf22d2c13e37e8f
andregorny <andre.gorny@gmail.com>
2023-02-14 19:31:54 -0300

    2023-02-14 uso no pc

---
+++ cliente.py
@@ -0,0 +1,13 @@
+class Cliente:
+
+    def __init__(self, nome: str, cpf: str,
+                 sexo: str, nascimento: str, 
+                 email: str, telefone: str, 
+                 cartao: str) -> None:
+        self.nome = nome
+        self.cpf = cpf
+        self.sexo = sexo
+        self.nascimento = nascimento
+        self.email = email
+        self.telefone = telefone
+        self.cartao = cartao
