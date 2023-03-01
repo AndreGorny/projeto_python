615ca68b0fde4a6d85966ba0c26f12ddd0f2c9f3
AndreGorny <andre.gorny@gmail.com>
2023-02-28 23:24:52 -0300

    2023-02-28 ajuste classe cliente

--- cliente.py
+++ cliente.py
@@ -1,13 +1,15 @@
-class Cliente:
+from dataclasses import dataclass
 
-    def __init__(self, nome: str, cpf: str,
-                 sexo: str, nascimento: str,
-                 email: str, telefone: str,
-                 cartao: str) -> None:
-        self.nome = nome
-        self.cpf = cpf
-        self.sexo = sexo
-        self.nascimento = nascimento
-        self.email = email
-        self.telefone = telefone
-        self.cartao = cartao
+
+@dataclass
+class Cliente:
+    nome: str
+    cpf: str
+    sexo: str
+    nascimento: str
+    email: str
+    telefone: str
+    cartao: str
+    saldo: str  # integer?
+    senha: str
+    endereco: object
